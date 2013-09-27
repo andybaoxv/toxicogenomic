a5f32f5240afcd21bef860f8814ebf7be032b19a
andybaoxv <changyale123@gmail.com>
2013-09-26 23:51:00 -0400

    Enable User Command Line

--- main_hierarchical.py
+++ main_hierarchical.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pickle
+from optparse import OptionParser
 
 from nitime import utils
 from nitime import algorithms as alg
@@ -12,6 +13,32 @@ from my_hierarchical_clustering import my_hierarchical_clustering
 from sklearn.metrics.pairwise import euclidean_distances
 import copy
 
+desc = """This script permits you to: 
+1) Extract features from the time series curve of each gene under each 
+condition(1 toxicant + 1 concentration). 
+Note that the user need to specify the order of auto-regressive model. 
+
+2) After feature extraction, Euclidean metric could be applied to generate
+a distance matrix, then hierarchical clustering is applied.
+
+3) The output is a file containing the dendrogram in png format.
+"""
+
+# User Interface with this program
+parser = OptionParser(description=desc)
+parser.add_option('--order',
+        help='Order of auto-regressive model.',
+        dest='n_ar_order',nargs=1,
+        metavar='<n_ar_order>',type='int',
+        default=3)
+parser.add_option('--linkage',
+        help='Linkage methods in hierarchical clustering:\
+                (single,complete,average)',
+        dest='method_linkage',nargs=1,
+        metavar='<method_linkage>',default='complete')
+
+(options, args) = parser.parse_args()
+
 # Read dataset and related information
 file_dataset = open("dataset_12dbps.pkl","rb")
 data,genes_name,pathways_type,toxicants_name = pickle.load(file_dataset)
@@ -22,13 +49,7 @@ n_toxicants,n_concentrations,n_genes,n_times = data.shape
 
 #########################################################################
 # Specify the order of autoregressive model
-n_ar_order = 3
-# Specify the number of clusters of conditions 
-n_clusters = 4
-# Apply PCA(True) or not(False)
-flag_pca = False
-# Visualization dimension(2 or 3)
-visual_dim = 3
+n_ar_order = options.n_ar_order
 #########################################################################
 
 # Fit the curve of each gene under each condition(1 toxicant+ 1 concentration)
@@ -47,7 +68,7 @@ for i in range(n_toxicants):
 
 # Obtain average noise level
 sigma_est_avg = sigma_est_avg/n_conditions
-print("AR order: "+str(n_ar_order)+"\tNoise level: "+str(sigma_est_avg))
+print("AR order: "+str(n_ar_order)+"\tAverage Noise level: "+str(sigma_est_avg))
 
 # n_instances = n_conditions, n_features = n_coefs
 n_instances,n_features = mtr_coef.shape
@@ -55,7 +76,7 @@ n_instances,n_features = mtr_coef.shape
 # Obtain distance matrix
 mtr_dis = euclidean_distances(mtr_coef,mtr_coef)
 mtr_sim = np.max(mtr_dis)-mtr_dis
-mtr_lin = my_hierarchical_clustering(mtr_sim,method='complete')
+mtr_lin = my_hierarchical_clustering(mtr_sim,method=options.method_linkage)
 
 # Draw dendrogram of hierarchical clustering
 fig = plt.figure(figsize=(30,30))
@@ -69,3 +90,4 @@ for i in range(tmp.shape[0]):
     tmp[i,2] = np.max(mtr_dis)-tmp[i,2]
 dend = dendrogram(tmp,labels=labels)
 plt.savefig('dendrogram_hierarchical.png')
+print("The result is stored in dendrogram_hierarchical.png")
